0205b09ffcf6fac7beed8f9e2cfa0cc057392ab4
Shane Mileham <shane.mileham@gmail.com>
2016-12-21 20:51:37 -0800

    Tested Stack (basic)

--- test/dataStructures/stack.test.js
+++ test/dataStructures/stack.test.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const chai = require('chai');
+const should = chai.should(); // eslint-disable-line no-unused-vars
+const Stack = require('../../dataStructures/stack.js');
+
 describe('Stack', function() {
   it('should just be an array', ()=>{});
+
+  it('should push and pop', ()=>{
+    let stack = new Stack();
+    stack.push(1);
+    stack.push('str');
+    stack.pop();
+    let one = stack.pop();
+    one.should.equal(1);
+  });
 });
