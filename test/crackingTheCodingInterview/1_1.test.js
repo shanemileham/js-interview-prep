a5c59e37c3af108dfc3fbba24ed2e31d4758cc0d
Shane Mileham <shane.mileham@gmail.com>
2016-12-22 01:49:20 -0800

    Completed c1q1

--- test/crackingTheCodingInterview/1_1.test.js
+++ test/crackingTheCodingInterview/1_1.test.js
@@ -6,7 +6,10 @@ What if you cannot use additional data structures?
 require('../bootstrap.test.js');
 
 describe('1.1 - Is Unique', function() {
-  it('should return true if all characters are unique');
+  it('should return true if all characters are unique', () => {
+    let string = 'qwerty';
+    c1q1(string).should.be.true;
+  });
   it('should return false if any character repeats', ()=>{
     let string = 'xqwertyuiopx';
     c1q1(string).should.be.false;
