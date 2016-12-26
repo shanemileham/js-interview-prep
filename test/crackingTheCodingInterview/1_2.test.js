0b3808cbbb082e6eab2f84f743891887251816d6
Shane Mileham <shane.mileham@gmail.com>
2016-12-26 11:18:53 -0800

    Ran test for 1.2 over both solutions

--- test/crackingTheCodingInterview/1_2.test.js
+++ test/crackingTheCodingInterview/1_2.test.js
@@ -6,31 +6,43 @@ if one is a permutation of the other.
 
 require('../bootstrap.test.js');
 
-describe('1.2 - Check Permutation', function() {
-  it('should return true if strings are identical', () => {
-    let stringA = 'qwerty';
-    let stringB = 'yqwetr';
-    c1q2(stringA, stringB).should.be.true;
-  });
-  it('should return true if strings are permutations', () => {
-    let stringA = 'qwerty';
-    let stringB = 'qwerty';
-    c1q2(stringA, stringB).should.be.true;
-  });
-  it('should return false if strings are different lengths', () => {
-    let stringA = 'abcdef';
-    let stringB = 'abcde';
-    c1q2(stringA, stringB).should.be.false;
-  });
-  it('should return false' +
-    'if strings have same letters but of different amounts', () => {
-    let stringA = 'abcdeef';
-    let stringB = 'abcdeff';
-    c1q2(stringA, stringB).should.be.false;
-  });
-  it('should return false if strings are completely different', () => {
-    let stringA = 'abcdef';
-    let stringB = 'ghijkl';
-    c1q2(stringA, stringB).should.be.false;
+describe('1.2 - Check Permutation', function(
+  solutions = [c1q2a, c1q2b]) {
+  solutions.forEach(function(solution) {
+    it(solution.name + ' ' +
+      'should return true\t' +
+      'if strings are identical', () => {
+      let stringA = 'qwerty';
+      let stringB = 'yqwetr';
+      solution(stringA, stringB).should.be.true;
+    });
+    it(solution.name + ' ' +
+      'should return true\t' +
+      'if strings are permutations', () => {
+      let stringA = 'qwerty';
+      let stringB = 'qwerty';
+      solution(stringA, stringB).should.be.true;
+    });
+    it(solution.name + ' ' +
+      'should return false\t' +
+      'if strings are different lengths', () => {
+      let stringA = 'abcdef';
+      let stringB = 'abcde';
+      solution(stringA, stringB).should.be.false;
+    });
+    it(solution.name + ' ' +
+      'should return false\t' +
+      'if strings have same letters but of different amounts', () => {
+      let stringA = 'abcdeef';
+      let stringB = 'abcdeff';
+      solution(stringA, stringB).should.be.false;
+    });
+    it(solution.name + ' ' +
+      'should return false\t' +
+      'if strings are completely different', () => {
+      let stringA = 'abcdef';
+      let stringB = 'ghijkl';
+      solution(stringA, stringB).should.be.false;
+    });
   });
 });
