4b9621857762521957cd664142c4d34371a5ba8f
Shane Mileham <shane.mileham@gmail.com>
2016-12-26 20:54:36 -0800

    Solved 1.4 with a basic solution

--- test/crackingTheCodingInterview/1_4.test.js
+++ test/crackingTheCodingInterview/1_4.test.js
@@ -10,12 +10,51 @@ require('../bootstrap.test.js');
 
 describe('1.4 - Palindrome Permutation', function() {
   c1q4.forEach(function(solution) {
+    /*
+    TRUE
+    123
+    123122
+    FALSE
+    12321
+    123321
+    21321
+    321321
+    */
     it(solution.name + ' ' +
       'should return false\t' +
-      'if strings are different lengths', () => {
-        const stringA = '1';
-        const stringB = '11';
-        solution(stringA, stringB).should.be.false;
+      'if input is 123', () => {
+        const input = '123';
+        solution(input).should.be.false;
+    });
+    it(solution.name + ' ' +
+      'should return false\t' +
+      'if input is 123122', () => {
+        const input = '123122';
+        solution(input).should.be.false;
+    });
+    it(solution.name + ' ' +
+      'should return true\t' +
+      'if input is 12321', () => {
+        const input = '12321';
+        solution(input).should.be.true;
+    });
+    it(solution.name + ' ' +
+      'should return true\t' +
+      'if input is 123321', () => {
+        const input = '123321';
+        solution(input).should.be.true;
+    });
+    it(solution.name + ' ' +
+      'should return true\t' +
+      'if input is 21321', () => {
+        const input = '21321';
+        solution(input).should.be.true;
+    });
+    it(solution.name + ' ' +
+      'should return true\t' +
+      'if input is 321321', () => {
+        const input = '321321';
+        solution(input).should.be.true;
     });
   });
 });
