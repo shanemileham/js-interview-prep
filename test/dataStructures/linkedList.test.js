fe97a051e44bd4c8c760a1aebfc95d6d74a5ea21
Shane Mileham <shane.mileham@gmail.com>
2017-01-01 16:56:58 -0800

    Changed print to toString for LinkedList

--- test/dataStructures/linkedList.test.js
+++ test/dataStructures/linkedList.test.js
@@ -8,34 +8,25 @@ describe('LinkedList', function() {
     sll.push(1);
     sll.push(2);
     sll.push('str');
-    sll.print();
-    console.log('Length: ' + sll.length());
+    sll.toString().should.equal('[1, 2, str]');
 
     // Pop 1
     const pop1 = sll.pop();
-    sll.print();
-    console.log('Length: ' + sll.length());
     pop1.should.equal('str');
     sll.length().should.equal(2);
 
     // Pop 2
     const pop2 = sll.pop();
-    sll.print();
-    console.log('Length: ' + sll.length());
     pop2.should.equal(2);
     sll.length().should.equal(1);
 
     // Pop 3
     const pop3 = sll.pop();
-    sll.print();
-    console.log('Length: ' + sll.length());
     pop3.should.equal(1);
     sll.length().should.equal(0);
 
     // Pop 4
     const pop4 = sll.pop();
-    sll.print();
-    console.log('Length: ' + sll.length());
     should.not.exist(pop4);
     sll.length().should.equal(0);
   });
