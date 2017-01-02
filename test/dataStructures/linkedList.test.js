216f4bcd3ee259332d0ef3a8bfaa8ce619413ff8
Shane Mileham <shane.mileham@gmail.com>
2017-01-01 16:53:38 -0800

    Added length and pop functions to LinkedList

--- test/dataStructures/linkedList.test.js
+++ test/dataStructures/linkedList.test.js
@@ -3,14 +3,40 @@ require('../bootstrap.test.js');
 describe('LinkedList', function() {
   it('should push and pop', ()=>{
     let sll = new LinkedList();
+
+    // Push three things
     sll.push(1);
     sll.push(2);
-    sll.push(4);
     sll.push('str');
     sll.print();
+    console.log('Length: ' + sll.length());
+
+    // Pop 1
+    const pop1 = sll.pop();
     sll.print();
-    // sll.pop();
-    // let one = sll.pop();
-    // one.should.equal(1);
+    console.log('Length: ' + sll.length());
+    pop1.should.equal('str');
+    sll.length().should.equal(2);
+
+    // Pop 2
+    const pop2 = sll.pop();
+    sll.print();
+    console.log('Length: ' + sll.length());
+    pop2.should.equal(2);
+    sll.length().should.equal(1);
+
+    // Pop 3
+    const pop3 = sll.pop();
+    sll.print();
+    console.log('Length: ' + sll.length());
+    pop3.should.equal(1);
+    sll.length().should.equal(0);
+
+    // Pop 4
+    const pop4 = sll.pop();
+    sll.print();
+    console.log('Length: ' + sll.length());
+    should.not.exist(pop4);
+    sll.length().should.equal(0);
   });
 });
